848589fcf37c5b66b13c18f9f508c9d4d54caf3e
Yousif Khalid <yousifkhalid544@gmail.com>
2025-10-06 10:39:28 -0700

    new project and setup GridView

---
+++ lib/categories.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('pick your meal')),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: const [
+          Text('data', style: TextStyle(color: Colors.amber),),
+          Text('data', style: TextStyle(color: Colors.amber),),
+          Text('data', style: TextStyle(color: Colors.amber),),
+          Text('data', style: TextStyle(color: Colors.amber),),
+          Text('data', style: TextStyle(color: Colors.amber),),
+          Text('data', style: TextStyle(color: Colors.amber),),
+        ],
+      ),
+    );
+  }
+}
